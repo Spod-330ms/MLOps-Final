c478d368898ee24ddad983da348244d63cf3e3ba
Michał Stanisławek <mistaniel@gmail.com>
2025-05-16 19:49:14 +0200

    base config

---
+++ ARISA_DSML/config.py
@@ -0,0 +1,38 @@
+"""Config file for module."""
+from pathlib import Path
+
+from dotenv import load_dotenv
+from loguru import logger
+
+# Load environment variables from .env file if it exists
+load_dotenv()
+
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
+DATASET = "wine_quality_classification"  # original competition dataset
+DATASET_TEST = "sahideseker/wine-quality-classification"  # test set augmented with target labels
+
+DATA_DIR = PROJ_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+EXTERNAL_DATA_DIR = DATA_DIR / "external"
+
+MODELS_DIR = PROJ_ROOT / "models"
+
+REPORTS_DIR = PROJ_ROOT / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
+
+MODEL_NAME = "wine-quality-bclass"
+
+categorical = [
+    "Pclass",
+    "Sex",
+    "Embarked",
+    "Deck",
+    "Title",
+]
+
+target = "Survived"
